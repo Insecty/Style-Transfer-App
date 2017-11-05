d9466f13d4864a2827285ea1f4b7e26c9edd3a9b
cy <617267602@qq.com>
2017-11-05 18:00:53 +0800

    can take photos or select pictures

---
+++ styletransfer/app/src/main/java/com/example/android/styletransfer/ChooseActivity.java
@@ -0,0 +1,145 @@
+package com.example.android.styletransfer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class ChooseActivity extends AppCompatActivity {
+
+    private ImageView temp;
+    private Button okbutton;
+
+    private static final int PHOTO_REQUEST_CAREMA = 1;// 拍照
+    private static final int PHOTO_REQUEST_GALLERY = 2;// 从相册中选择
+    private static final int PHOTO_REQUEST_CUT = 3;// 结果
+
+    /* 头像名称 */
+    private static final String PHOTO_FILE_NAME = "temp_photo.jpg";
+    private File tempFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+
+        WindowManager wm = this.getWindowManager();
+        int width = wm.getDefaultDisplay().getWidth();
+        temp = (ImageView)findViewById(R.id.imgview);
+        LayoutParams lp=(LayoutParams) temp.getLayoutParams();
+        lp.width=width;
+        lp.height=width;
+        temp.setLayoutParams(lp);
+
+        isBinding();
+        onClick();
+
+    }
+
+    private void isBinding(){
+        okbutton=(Button) findViewById(R.id.okbutton);
+    }
+
+    private void onClick(){
+        okbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(ChooseActivity.this,StyleActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void gallery(View view) {
+        // 激活系统图库，选择一张图片
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        // 开启一个带有返回值的Activity，请求码为PHOTO_REQUEST_GALLERY
+        startActivityForResult(intent, PHOTO_REQUEST_GALLERY);
+    }
+
+    public void camera(View view){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        if(hasSdcard()){
+            tempFile = new File(Environment.getExternalStorageDirectory(),PHOTO_FILE_NAME);
+            Uri uri = Uri.fromFile(tempFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        }
+        // 开启一个带有返回值的activity
+        startActivityForResult(intent, PHOTO_REQUEST_CAREMA);
+    }
+
+    private boolean hasSdcard() {
+        if (Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void crop(Uri uri) {
+        // 裁剪图片意图
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        // 裁剪框的比例，1：1
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // 裁剪后输出图片的尺寸大小
+        intent.putExtra("outputX", 350);
+        intent.putExtra("outputY", 350);
+
+        intent.putExtra("outputFormat", "JPEG");// 图片格式
+        intent.putExtra("noFaceDetection", true);// 取消人脸识别
+        intent.putExtra("return-data", true);
+        // 开启一个带有返回值的Activity，请求码为PHOTO_REQUEST_CUT
+        startActivityForResult(intent, PHOTO_REQUEST_CUT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PHOTO_REQUEST_GALLERY) {
+            // 从相册返回的数据
+            if (data != null) {
+                // 得到图片的全路径
+                Uri uri = data.getData();
+                crop(uri);
+            }
+        } else if (requestCode == PHOTO_REQUEST_CAREMA) {
+            // 从相机返回的数据
+            if (hasSdcard()) {
+                crop(Uri.fromFile(tempFile));
+            } else {
+                Toast.makeText(ChooseActivity.this, "未找到存储卡，无法存储照片！", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == PHOTO_REQUEST_CUT) {
+            // 从剪切图片返回的数据
+            if (data != null) {
+                Bitmap bitmap = data.getParcelableExtra("data");
+                this.temp.setImageBitmap(bitmap);
+            }
+            try {
+                // 将临时文件删除
+                tempFile.delete();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
--- styletransfer/app/src/main/java/com/example/android/styletransfer/MainActivity.java
+++ styletransfer/app/src/main/java/com/example/android/styletransfer/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.android.styletransfer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isBinding();
+        onClick();
+    }
+
+    private void isBinding(){
+        btn1 = (Button) findViewById(R.id.beginbutton);
+    }
+
+    private void onClick(){
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,ChooseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
