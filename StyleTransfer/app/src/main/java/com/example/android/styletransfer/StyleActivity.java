d00c5ddda84372bcca52346a0e9a45693e819e04
cy <617267602@qq.com>
2017-11-11 22:42:21 +0800

    still some bugs

--- StyleTransfer/app/src/main/java/com/example/android/styletransfer/StyleActivity.java
+++ StyleTransfer/app/src/main/java/com/example/android/styletransfer/StyleActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.styletransfer;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
@@ -12,13 +11,30 @@ import android.view.ViewGroup.LayoutParams;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+ import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Button;
+
 
 public class StyleActivity extends AppCompatActivity {
 
     private ImageView temp;
     private SeekBar seekbar;
     public Bitmap bitmap;
+    private Bitmap srcBitmap;
     private TextView seekbarnum;
+    public int size;
+    private ImageView dstimage = null;
+    //private int flag=-1;//1代表亮度，2代表对比度，3代表饱和度，4代表黑白处理
+    //private Button button1,button2,button3,button4;
+    //private Button[] button={button1,button2,button3,button4};//同上对应
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +46,6 @@ public class StyleActivity extends AppCompatActivity {
         temp = (ImageView)findViewById(R.id.imgview);
         LayoutParams lp=(LayoutParams) temp.getLayoutParams();
         lp.width=width;
-        lp.height=width;
         temp.setLayoutParams(lp);
 
         temp=(ImageView)findViewById(R.id.imgview);
@@ -41,29 +56,162 @@ public class StyleActivity extends AppCompatActivity {
         }else if(intent == null){
             Toast.makeText(StyleActivity.this, "图片传输失败了！", Toast.LENGTH_SHORT).show();
         }
-
+        dstimage = (ImageView) findViewById(R.id.imgview);
+        srcBitmap=bitmap;
+        size=srcBitmap.getWidth();
         isBinding();
-        onClick();
+
     }
 
     private void isBinding(){
         seekbar = (SeekBar) findViewById(R.id.seekbar);
         seekbarnum = (TextView) findViewById(R.id.seekbarnum);
+        //button1 = (Button) findViewById(R.id.light);
+        //button2= (Button) findViewById(R.id.contrast);
+        //button3= (Button) findViewById(R.id.saturation);
+        //button4 = (Button) findViewById(R.id.baw);
+
     }
 
-    private void onClick(){
+    public void light(View view){
+        //按钮显示设置
+
+        //进度条调节
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             // 数值改变
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 seekbarnum.setText(""+i);
+                Bitmap bmp = Bitmap.createBitmap(size, size, Config.ARGB_8888);
+                //获取实际用来应用的数值
+                int brightness = i-50;//亮度数值
+                ColorMatrix cMatrix = new ColorMatrix();
+                //利用数值创建颜色矩阵
+                cMatrix.set(new float[] { 1, 0, 0, 0, brightness, 0, 1, 0, 0, brightness,0, 0, 1, 0, brightness, 0, 0, 0, 1, 0 });//亮度矩阵
+                //创建画笔
+                Paint paint = new Paint();
+                //设定矩阵
+                paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
+                //创建画布
+                Canvas canvas = new Canvas(bmp);
+                //绘制
+                canvas.drawBitmap(srcBitmap, 0, 0, paint);
+                dstimage.setImageBitmap(bmp);
             }
-            // 开始拖动
+            // 开始拖动（不用写任何东西）
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            // 停止拖动（不用写任何东西）
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
-            // 停止拖动
+        });
+    }
+
+
+    public void contrast(View view){
+        //按钮显示设置
+
+        //进度条调节
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // 数值改变
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekbarnum.setText(""+i);
+                Bitmap bmp = Bitmap.createBitmap(size, size, Config.ARGB_8888);
+                //获取实际用来应用的数值
+                float contrast = (float) ((i + 50) /100.0);//对比度数值
+                ColorMatrix cMatrix = new ColorMatrix();
+                //利用数值创建颜色矩阵
+                cMatrix.set(new float[] { contrast, 0, 0, 0, 0, 0, contrast, 0, 0, 0, 0, 0, contrast, 0, 0, 0, 0, 0, 1, 0 });//对比度矩阵
+                //创建画笔
+                Paint paint = new Paint();
+                //设定矩阵
+                paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
+                //创建画布
+                Canvas canvas = new Canvas(bmp);
+                //绘制
+                canvas.drawBitmap(srcBitmap, 0, 0, paint);
+                dstimage.setImageBitmap(bmp);
+            }
+            // 开始拖动（不用写任何东西）
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            // 停止拖动（不用写任何东西）
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+
+    public void saturation(View view){
+        //按钮显示设置
+
+        //进度条调节
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // 数值改变
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekbarnum.setText(""+i);
+                Bitmap bmp = Bitmap.createBitmap(size, size, Config.ARGB_8888);
+                ColorMatrix cMatrix = new ColorMatrix();
+                //利用数值创建颜色矩阵
+                cMatrix.setSaturation((float) (i *2.25 / 100.0));//饱和度矩阵
+                //创建画笔
+                Paint paint = new Paint();
+                //设定矩阵
+                paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
+                //创建画布
+                Canvas canvas = new Canvas(bmp);
+                //绘制
+                canvas.drawBitmap(srcBitmap, 0, 0, paint);
+                dstimage.setImageBitmap(bmp);
+            }
+            // 开始拖动（不用写任何东西）
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            // 停止拖动（不用写任何东西）
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void baw(View view){
+        //按钮显示设置
+
+        //进度条调节
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            // 数值改变
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                seekbarnum.setText(""+i);
+                Bitmap bmp = Bitmap.createBitmap(size, size, Config.ARGB_8888);
+                ColorMatrix cMatrix = new ColorMatrix();
+                //利用数值创建颜色矩阵
+                cMatrix.setSaturation(0);//黑白（即饱和度为0）
+                //创建画笔
+                Paint paint = new Paint();
+                //设定矩阵
+                paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
+                //创建画布
+                Canvas canvas = new Canvas(bmp);
+                //绘制
+                canvas.drawBitmap(srcBitmap, 0, 0, paint);
+                dstimage.setImageBitmap(bmp);
+            }
+            // 开始拖动（不用写任何东西）
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            // 停止拖动（不用写任何东西）
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
--- styletransfer/app/src/main/java/com/example/android/styletransfer/MainActivity.java
+++ styletransfer/app/src/main/java/com/example/android/styletransfer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.styletransfer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,21 +17,33 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         isBinding();
-        onClick();
+        ButtonListner b= new ButtonListner();
+        btn1.setOnClickListener(b);
+        btn1.setOnTouchListener(b);
+        btn1.setBackgroundResource(R.drawable.beginbutton_up);
     }
 
-    private void isBinding(){
+    private void isBinding() {
         btn1 = (Button) findViewById(R.id.beginbutton);
     }
 
-    private void onClick(){
-        btn1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent=new Intent(MainActivity.this,ChooseActivity.class);
-                startActivity(intent);
+    class ButtonListner implements View.OnClickListener, View.OnTouchListener{
+
+        public void onClick(View V) {
+            Intent intent = new Intent(MainActivity.this, ChooseActivity.class);
+            startActivity(intent);
+        }
+
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                btn1.setBackgroundResource(R.drawable.beginbutton_up);
             }
-        });
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                btn1.setBackgroundResource(R.drawable.beginbutton_down);
+            }
+            return false;
+        }
     }
 }
